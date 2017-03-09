d58e05a03dae9a33d245b63099af2f1b71f66725
mulhern <amulhern@redhat.com>
2017-03-09 11:09:06 -0500

    Make a new _prop_builder
    Remove everything else.
    
    The new version takes an XML spec as its spec and modifies the
    namespace so that each property has its own separate class.
    
    Signed-off-by: mulhern <amulhern@redhat.com>

--- src/dbus_python_client_gen/_invokers.py
+++ src/dbus_python_client_gen/_invokers.py
@@ -6,85 +6,51 @@
 Code for generating classes suitable for invoking dbus-python methods.
 """
 
-_IDENTITY = lambda x: x # pragma: no cover
+from ._errors import DPClientGenerationError
 
-def _option_to_tuple(value, default):
-    """
-    Converts a Python "option" type, None or a value, to a tuple.
-
-    :param object value: any value
-    :param object default: a valid default to use if value is None
-    :returns: a tuple encoding the meaning of value
-    :rtypes: bool * object
-    """
-    return (False, default) if value is None else (True, value)
-
-
-def _info_to_xformer(specs):
-    """
-    Function that yields a xformer function.
-
-    :param specs: specifications for this function
-    :type names: iterable of triples, name, inxform function, sig
-    :return: a transformation function
-    :rtype: (list of object) -> (list of object)
-    """
-    inxforms = [y for (_, y, _) in specs]
-    outxforms = [f for (f, _) in xformers("".join(z for (_, _, z) in specs))]
-    expected_length = len(specs)
-
-    def xformer(objects):
-        """
-        Transforms a list of objects to dbus-python types.
-
-        :param objects: list of objects
-        :type objects: list of object
-        :returns: a transformed list of objects
-        :rtype: list of object
-        """
-        if len(objects) != expected_length:
-            raise ValueError("wrong number of objects")
-
-        return \
-           [x for (x, _) in (f(g(a)) for \
-           ((f, g), a) in zip(zip(outxforms, inxforms), objects))]
-
-    return xformer
-
-
-def _xformers(key_to_sig):
-    """
-    Get a map from keys to functions from a map of names to signatures.
-
-    :param key_to_sig: a map from keys to signatures
-    :type key_to_sig: dict of object * str
-    :returns: a map from keys to functions
-    :rtype: dict of object * xformation function
-    """
-    return dict(
-       (method, ([n for (n, _, _) in specs], _info_to_xformer(specs))) for \
-       (method, specs) in key_to_sig.items())
 
 def _prop_builder(spec):
     """
     Returns a function that builds a property interface based on 'spec'.
 
     :param spec: the interface specification
-    :type spec: type, a subtype of InterfaceSpec
+    :type spec: xml.element.ElementTree.Element
+
+    :raises DPClientGenerationError:
     """
+
+    interface_name = spec.find("./[@name]")
+    if interface_name is None:
+        raise DPClientGenerationError("No name found for interface.")
 
     def builder(namespace):
         """
         The property class's namespace.
 
+        Fills the namespace of the parent class with class members that are
+        classes. Each class member has the name of a property, and each
+        class has up to two static methods, a Get method if the property is
+        readable and a Set method if the property is writable.
+
+        For example, given the spec:
+
+        <property name="Version" type="s" access="read">
+            <annotation
+                name="org.freedesktop.DBus.Property.EmitsChangedSignal"
+                value="const"/>
+        </property>
+
+        A class called "Version" with a single method "Get" will be added to the
+        namespace.
+
         :param namespace: the class's namespace
         """
 
-        def build_property(prop): # pragma: no cover
+        def build_property_getter(name): # pragma: no cover
             """
             Build a single property getter for this class.
 
-            :param prop: the property
+            :param str name: the property name
             """
 
             def dbus_func(proxy_object): # pragma: no cover
@@ -92,68 +58,75 @@ def _prop_builder(spec):
                 The property getter.
                 """
                 return proxy_object.Get(
-                   spec.INTERFACE_NAME,
-                   prop.name,
+                   interface_name,
+                   name,
                    dbus_interface=dbus.PROPERTIES_IFACE
                 )
 
             return dbus_func
 
-        for prop in spec.PropertyNames:
-            namespace[prop.name] = staticmethod(build_property(prop)) # pragma: no cover
-
-    return builder
-
-
-def _iface_builder(spec):
-    """
-    Returns a function that builds a method interface based on 'spec'.
-
-    :param spec: the interface specification
-    :type spec: type, a subtype of InterfaceSpec
-    """
-
-    def builder(namespace):
-        """
-        Builds the class.
-
-        :param namespace: the class's namespace
-        """
-
-        def build_method(method):
+        def build_property_setter(name): # pragma: no cover
             """
-            Build a single method for this class.
+            Build a single property setter for this class.
 
-            :param method: the method
+            :param str name: the property name
             """
-            (names, func) = spec.XFORMERS[method]
 
-            def dbus_func(proxy_object, **kwargs): # pragma: no cover
+            def dbus_func(proxy_object, value): # pragma: no cover
                 """
-                The function method spec.
+                The property setter.
                 """
-                if frozenset(names) != frozenset(kwargs.keys()):
-                    raise ValueError("Bad keys")
-                args = \
-                   [v for (k, v) in \
-                   sorted(kwargs.items(), key=lambda x: names.index(x[0]))]
-                xformed_args = func(args)
-                dbus_method = getattr(proxy_object, method.name)
-                return dbus_method(
-                   *xformed_args,
-                   dbus_interface=spec.INTERFACE_NAME
+                return proxy_object.Set(
+                   interface_name,
+                   name,
+                   value,
+                   dbus_interface=dbus.PROPERTIES_IFACE
                 )
 
             return dbus_func
 
-        for method in spec.MethodNames:
-            namespace[method.name] = staticmethod(build_method(method))
+        for prop in spec.findall('./property'):
+            name = prop.find('./[@name]')
+            if name is None:
+                raise DPClientGenerationError("No name found for property.")
 
-        namespace['Properties'] = \
-           types.new_class(
-              "Properties",
-              bases=(object,),
-              exec_body=_prop_builder(spec)
-           )
+            access = prop.find('./[@access]')
+            if access is None:
+                raise DPClientGenerationError("No access found for property.")
+
+            if access == "read":
+                getter = staticmethod(build_property_getter(name))
+
+                def method_builder(namespace):
+                    """
+                    Attaches appropriate methods to namespace.
+                    """
+                    namespace['Get'] = getter
+
+            elif access == "write":
+                setter = staticmethod(build_property_setter(name))
+
+                def method_builder(namespace):
+                    """
+                    Attaches appropriate methods to namespace.
+                    """
+                    namespace['Set'] = setter
+            else:
+                getter = staticmethod(build_property_getter(name))
+                setter = staticmethod(build_property_setter(name))
+
+                def method_builder(namespace):
+                    """
+                    Attaches appropriate methods to namespace.
+                    """
+                    namespace['Get'] = getter
+                    namespace['Set'] = setter
+
+            namespace[name] = \
+               types.new_class(
+                  name,
+                  bases=(object,),
+                  exec_body=method_builder
+               )
 
     return builder
