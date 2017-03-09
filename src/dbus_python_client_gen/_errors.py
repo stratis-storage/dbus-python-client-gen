5fab5508f5710c764f88bc27378711128df5f962
mulhern <amulhern@redhat.com>
2017-03-09 10:14:34 -0500

    Add a few error classes
    Signed-off-by: mulhern <amulhern@redhat.com>

---
+++ src/dbus_python_client_gen/_errors.py
@@ -0,0 +1,19 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+"""
+Exception hierarchy for this package.
+"""
+
+class DPClientError(Exception):
+    """
+    Top-level error.
+    """
+    pass
+
+class DPClientGenerationError(Exception):
+    """
+    Exception during generation of classes.
+    """
+    pass
